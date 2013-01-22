de56c48394ce03c42fcc180b268293aa4c08a97e
Nick Van Walleghem <nickvw91@hotmail.com>
2013-01-22 13:10:35 +0100

    Taxi besteld

---
+++ application/views/v_taxibesteld.php
@@ -0,0 +1,42 @@
+	<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<head>
+	<title>Admin TaxiNu</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>stylesheets/foundation.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>stylesheets/app.css">
+   <link rel="stylesheet/less" type="text/css" href="<?php echo base_url();?>stylesheets/styles.less">
+   <link rel="stylesheet" media="all" type="text/css" href="http://code.jquery.com/ui/1.9.1/themes/smoothness/jquery-ui.css" />
+    
+  <script src="<?php echo base_url();?>javascripts/less.js" type="text/javascript"></script>
+  <script src="<?php echo base_url();?>javascripts/modernizr.foundation.js"></script>
+
+</head>
+<body>
+<header>
+		   
+		<div class="row">
+		
+			<div class="twelve colums">
+				
+				<div id="loading">
+					<h3>Uw taxi is besteld en onderweg. </h3>
+				<br>
+				<br>
+				</div>
+			</div>
+		</div>
+		<div class="nav">
+			 <a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>images/logo.png" width="200px"></a>			 <div class="social">
+			 	<img src="<?php echo base_url();?>images/f.png" width="15px;">
+			 	<img src="<?php echo base_url();?>images/t.png" width="50px;">
+			 	<img src="<?php echo base_url();?>images/p.png" width="25px;">
+			 	
+			 </div>
+		</div>
+</header>
+	
+		 <script src="http://js.pusher.com/1.12/pusher.min.js" type="text/javascript"></script>
+	 <script src="<?php echo base_url();?>javascripts/jquery.js"></script>
+	 <script src="<?php echo base_url();?>javascripts/pusher_client.js"></script>
+</body>
