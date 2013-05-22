1f3a045f40adcd41c497269d595f299c74698efa
jeroenwouters <jeroen.wou@gmail.com>
2013-05-22 15:00:47 +0200

    Admin moble map!
    Map location taxi;
    map pins bestellingen;

--- application/views/v_admin_mobile.php
+++ application/views/v_admin_mobile.php
@@ -1,8 +1,15 @@
 
 	<header> 
-		<img src="<?php echo base_url();?>images/logo.png" width="180px">		
+		<img id="set" class="icon lefticon" id="world" src="<?php echo base_url();?>images/set.png">
+		<img id="list" class="icon lefticon" id="world" src="<?php echo base_url();?>images/list.png">
+		<img id="logo" src="<?php echo base_url();?>images/logo.png" width="180px">
+		<img id="world" class="icon righticon"  src="<?php echo base_url();?>images/world.png">	
+		<img id="loc" class="icon righticon"  src="<?php echo base_url();?>images/loc.png">	
 	</header>
 	
-	<div class="wrapper">
+	<div id="lijst" class="wrapper">
 			<ul class="ritten"></ul>
+	</div>
+	<div class="wrapper" id="map">
+			
 	</div>
